26360a1224ed20d60d47a7b3f20f8afcbf612411
Akshay <akshayjha@live.in>
2018-07-02 22:12:16 +0530

    new file

---
+++ 1_tfbasics.py
@@ -0,0 +1,22 @@
+# Just disables the warning, doesn't enable AVX/FMA
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["TF_CPP_MIN_LOG_LEVEL"]= "3"
+
+
+import tensorflow as tf
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+
+result = x1 * x2
+print(result)
+
+sess = tf.Session()
+print(sess.run(result))
+sess.close()
+
+with tf.Session() as sess:
+	output = sess.run(result)
+	print(output)
+
+print(output)	
